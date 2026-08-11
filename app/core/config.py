a97bab1a88331843b71c14bdf7e70032c86c9fb9
jcal61 <josephjohncal@gmail.com>
2026-08-10 23:42:58 -0500

    Delete app directory

--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:////tmp/craftable_replica.db"
-    APP_NAME: str = "Craftable Replica"
-    DEBUG: bool = True
-
-settings = Settings()
